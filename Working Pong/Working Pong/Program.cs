530d9f77b0d0e01f30f2d577b897c7fb9940bdb3
Wyatt <matusewi.w@stu.janesville.k12.wi.us>
2024-12-19 09:35:46 -0600

    Classwork Update

---
+++ Working Pong/Working Pong/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: matusewi.w
+ * Date: 12/11/2024
+ * Time: 8:53 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Working_Pong
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
